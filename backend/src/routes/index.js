821c0005330d3f9b6d3333f5d902ee4bfe543f48
rahul kumar <rkrahul8181@gmail.com>
2023-06-02 08:57:08 +0530

    Create user route

---
+++ backend/src/routes/index.js
@@ -0,0 +1,7 @@
+const {Router} = require('express')
+
+const app = Router();
+
+app.post('/login',()=>{})
+
+module.exports = app
--- backend/src/server.js
+++ backend/src/server.js
@@ -3,9 +3,13 @@ const cors = require('cors')
 const app = express();
 const {PORT} = require("./config")
 const connect = require('./database/connect')
+
+const userRouter = require('./routes')
 // Default Middleware
 app.use(express.json());
 app.use(cors())
+
+app.use("/api",userRouter)
 
 app.get("/",(req,res)=>{
     res.send("Working fine")
